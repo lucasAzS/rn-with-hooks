2d9976602bb7aa61c1d82db6a6e2da22036ae230
Lucas de Azambuja dos Santos <lucasazsantos@gmail.com>
2021-08-20 14:09:06 -0300

    using reducer with community convention

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const INCREMENT = 1;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increase_counter':
+      return { ...state, counter: state.counter + action.payload };
+    case 'decrease_counter':
+      return { ...state, counter: state.counter + action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = React.useState(0);
+  const [state, dispatch] = React.useReducer(reducer, { counter: 0 });
 
   return (
     <View>
-      <Button title='Increase' onPress={() => setCounter(counter + 1)} />
-      <Button title='Decrease' onPress={() => setCounter(counter - 1)} />
-      <Text>Current Count: {counter}</Text>
+      <Button
+        title='Increase'
+        onPress={() =>
+          dispatch({ type: 'increase_counter', payload: INCREMENT })
+        }
+      />
+      <Button
+        title='Decrease'
+        onPress={() =>
+          dispatch({ type: 'decrease_counter', payload: -INCREMENT })
+        }
+      />
+      <Text>Current Count: {state.counter}</Text>
     </View>
   );
 };
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,21 +5,22 @@ import ColorCounter from '../components/ColorCounter';
 const COLOR_INCREMENT = 15;
 
 const reducer = (state, action) => {
-  switch (action.colorToChange) {
-    case 'red':
-      return state.red + action.amount > 255 || state.red + action.amount < 0
+  switch (action.type) {
+    case 'change_red':
+      return state.red + action.payload > 255 || state.red + action.payload < 0
         ? state
-        : { ...state, red: state.red + action.amount };
+        : { ...state, red: state.red + action.payload };
 
-    case 'green':
-      return state.green + action.amount > 255 ||
-        state.green + action.amount < 0
+    case 'change_green':
+      return state.green + action.payload > 255 ||
+        state.green + action.payload < 0
         ? state
-        : { ...state, green: state.green + action.amount };
-    case 'blue':
-      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        : { ...state, green: state.green + action.payload };
+    case 'change_blue':
+      return state.blue + action.payload > 255 ||
+        state.blue + action.payload < 0
         ? state
-        : { ...state, blue: state.blue + action.amount };
+        : { ...state, blue: state.blue + action.payload };
     default:
       return state;
   }
@@ -37,28 +38,28 @@ const SquareScreen = () => {
     <View>
       <ColorCounter
         onIncrease={() =>
-          dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })
+          dispatch({ type: 'change_red', payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: 'red', amount: -COLOR_INCREMENT })
+          dispatch({ type: 'change_red', payload: -COLOR_INCREMENT })
         }
         color='Red'
       />
       <ColorCounter
         onIncrease={() =>
-          dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })
+          dispatch({ type: 'change_green', payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: 'green', amount: -COLOR_INCREMENT })
+          dispatch({ type: 'change_green', payload: -COLOR_INCREMENT })
         }
         color='Green'
       />
       <ColorCounter
         onIncrease={() =>
-          dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })
+          dispatch({ type: 'change.blue', payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: 'blue', amount: -COLOR_INCREMENT })
+          dispatch({ type: 'change.blue', payload: -COLOR_INCREMENT })
         }
         color='Blue'
       />
